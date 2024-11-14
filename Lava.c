7304e9349ed51cdee5aaa2eb0323570465e06773
Lava195 <bt23ece129@iiitn.ac.in>
2024-11-14 21:52:39 +0530

    Create Lava.c

---
+++ Lava.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+  int a,b,c;
+printf("enter the first number");
+scanf("%d",&a);
+printf("enter the second number");
+scanf("%d",&b);
+c=a+b;
+printf("%d",c");
+return 0;
+}
